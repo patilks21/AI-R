484bf3e0aba9bfec804367f78f9cd71f8281508d
patilks21 <46299620+patilks21@users.noreply.github.com>
2019-08-07 12:25:25 +0530

    Add files via upload

---
+++ bestfs.py
@@ -0,0 +1,122 @@
+
+class Node:
+
+	def __init__(self, f=0, g=999999, h=0, name=0):
+		self.f = f
+		self.g = g
+		self.h = h
+		self.name = name
+	
+	def setNeighbours(self, neighbours={}):
+		self.neighbours = neighbours
+
+
+
+graph=[]
+heuristics=[]
+
+n=int(input("no of nodes"))
+for i in range(n):
+	z=[]
+	graph.append(z)
+	for j in range(n):
+		graph[i].append(int(input()))
+for i in range(n):
+	heuristics.append(int(input("heuristic value")))
+Name=[]
+for i in range(n):
+	a=Node(h=heuristics[i], name=str(i))
+	Name.append(a)
+for i in range(n):
+	ls=[]
+	for j in range(n):
+		if graph[i][j]!=0:
+			ls.append(Name[j])
+	Name[i].setNeighbours(ls)
+
+
+
+startNode = Name[int(input("Enter start node"))]
+goalNode = Name[int(input("Enter goal node"))]
+
+
+def astar(start,goal):
+
+	closedSet = set([])
+	openSet = set([start])
+
+	cameFrom = {}
+	
+	start.g = 0
+	start.f = start.h
+
+
+	while len(openSet)!=0:
+
+		current = findNodeWithLowestFScore(openSet)
+
+		if current==goal:
+			return contruct_path(cameFrom, current)
+
+		openSet.remove(current)
+		closedSet.add(current)
+
+		#print(current.name, current.f, current.g, current.h)
+
+		for neighbour in current.neighbours:
+
+			#print(neighbour.name, neighbour.f, neighbour.g, neighbour.h)
+
+			if neighbour in closedSet:
+				continue
+
+			if neighbour not in openSet:
+				openSet.add(neighbour)
+
+
+			tentative_gScore = current.g 
+			#print(tentative_gScore)
+			if tentative_gScore >= neighbour.g:
+				continue
+
+			cameFrom[neighbour] = current
+			neighbour.g = tentative_gScore
+			neighbour.f = neighbour.h
+
+
+
+	return -1
+
+def findNodeWithLowestFScore(openSet):
+	fScore = 999999
+	node = None
+	for eachNode in openSet:
+		if eachNode.f < fScore:
+			fScore = eachNode.f
+			node = eachNode
+
+	return node
+
+
+def contruct_path(cameFrom, current):
+
+	totalPath = []
+	while current in cameFrom.keys():
+		current = cameFrom[current]
+		totalPath.append(current)
+
+	return totalPath
+
+
+
+if __name__=="__main__":
+	
+	
+	path = astar(startNode, goalNode)
+
+	print("Path is : ", end="" )
+	for node in path[::-1]:
+		print(str(node.name) + "-->", end="")	
+	print(goalNode.name)
+
+
